9cc39968b337afd76d0b88e3e95f85f3bd132c58
Paloma Peralta <palomaperalta9@gmail.com>
2022-09-09 19:39:15 -0300

    header

--- src/App.js
+++ src/App.js
@@ -15,8 +15,48 @@ function App() {
     getCats()
   },[])
 
+  function averageLife() {
+    const catsage = cats.reduce((acc, cat) =>{
+       const life = cat.life_span
+       const splited = life.split("-")
+       return parseInt(splited[0]) + acc 
+    }, 0)
+
+    return catsage / cats.length
+  }
+    function averageWeight(){
+      const catsweight = cats.reduce((acc, cat) =>{
+        const weight = cat.weight.metric
+        const splited = weight.split("-")
+        return parseInt(splited[0]) + acc
+      }, 0)
+      return catsweight / cats.length
+    }
+    console.log(averageWeight())
   return (
     <div className="App">
+    <div className="header">
+      <div className="title">
+        <h1>
+          30 DAYS OF REACT
+        </h1>
+        <div className="day"> 
+          DAY 20
+        </div>
+      </div>
+      <div className="cats-paradise">
+      <h1>
+        Cats Paradise
+      </h1>
+      <p className="breeds">
+        There are {cats.length} cats breeds
+      </p>
+      <p>
+        On average a cat can weight about {Math.floor(averageWeight())} Kg and lives {Math.floor(averageLife())} years.
+      </p>
+    </div>
+    </div>
+    
     {cats.map(cat => {
       console.log(cat)
       return (
