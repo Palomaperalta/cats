604f1f71358202b423b8a6542419fdfbf0cdfb74
Paloma Peralta <palomaperalta9@gmail.com>
2024-09-09 17:27:15 +0200

    add api key

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,72 +1,79 @@
 /** @jsxImportSource @emotion/react */
-import CatsCard from '../components/CatsCard';
-import {useState, useEffect, useContext} from 'react';
-import { ThemeContext } from './../App';
-import MainLayout from './../components/common/MainLayout';
-import CatsSummary from './../components/CatsSummary';
-import FilterCat from './../components/FilterCat';
-import * as styles from './Home.styles'
+import CatsCard from "../components/CatsCard";
+import { useState, useEffect, useContext } from "react";
+import { ThemeContext } from "./../App";
+import MainLayout from "./../components/common/MainLayout";
+import CatsSummary from "./../components/CatsSummary";
+import FilterCat from "./../components/FilterCat";
+import * as styles from "./Home.styles";
 
 function Home() {
-  const [cats, setCats] = useState([])
-  const [loading, setLoading] = useState(false)
-  const [originalCats, setOriginalCats] = useState([])
-  const [filters, setFilters] = useState({country: "all", name: ""})
+  const [cats, setCats] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [originalCats, setOriginalCats] = useState([]);
+  const [filters, setFilters] = useState({ country: "all", name: "" });
   const [theme] = useContext(ThemeContext);
 
-  async function getCats(){
-    setLoading(true)
-    const response = await fetch('https://api.thecatapi.com/v1/breeds')
-    const catsApi = await response.json()
-    setCats(catsApi)
-    setOriginalCats(catsApi)
-    setLoading(false)
+  async function getCats() {
+    setLoading(true);
+    const response = await fetch(
+      `https://api.thecatapi.com/v1/breeds?api_key=${process.env.REACT_APP_API_KEY}`
+    );
+    const catsApi = await response.json();
+    setCats(catsApi);
+    setOriginalCats(catsApi);
+    setLoading(false);
   }
 
-  useEffect(()=>{
-    getCats()
-  },[])
+  useEffect(() => {
+    getCats();
+  }, []);
 
-  useEffect(()=>{
-    const filteredCats = originalCats.filter(cat => {
+  useEffect(() => {
+    const filteredCats = originalCats.filter((cat) => {
       if (filters.country === "all") {
-        return cat.name.toLowerCase().includes(filters.name.toLowerCase())
+        return cat.name.toLowerCase().includes(filters.name.toLowerCase());
       } else {
-        return cat.origin === filters.country && cat.name.toLowerCase().includes(filters.name.toLowerCase())
+        return (
+          cat.origin === filters.country &&
+          cat.name.toLowerCase().includes(filters.name.toLowerCase())
+        );
       }
-    })
-    setCats(filteredCats)
-  },[filters, originalCats, setCats])
+    });
+    setCats(filteredCats);
+  }, [filters, originalCats, setCats]);
 
   return (
-      <MainLayout>
-        <CatsSummary cats={cats}/>
-        <div css={styles.homepage(theme)}>
-          <FilterCat 
-            setFilters={setFilters}
-            filters={filters}
-            originalCats={originalCats} 
-          />
-          
-          {loading ? <span>Loading...</span> :
-            cats.map(cat => {
-              return (
-                <CatsCard
-                  origin={cat.origin}
-                  name={cat.name}
-                  img={cat.image?.url}
-                  description={cat.description}
-                  key={cat.id} 
-                  temperament={cat.temperament}
-                  lifespan={cat.life_span}
-                  weight={cat.weight.metric}
-                />
-              )
-            })} 
-          </div>
-        </MainLayout>
+    <MainLayout>
+      <CatsSummary cats={cats} />
+      <div css={styles.homepage(theme)}>
+        <FilterCat
+          setFilters={setFilters}
+          filters={filters}
+          originalCats={originalCats}
+        />
+
+        {loading ? (
+          <span>Loading...</span>
+        ) : (
+          cats.map((cat) => {
+            return (
+              <CatsCard
+                origin={cat.origin}
+                name={cat.name}
+                img={cat.image?.url}
+                description={cat.description}
+                key={cat.id}
+                temperament={cat.temperament}
+                lifespan={cat.life_span}
+                weight={cat.weight.metric}
+              />
+            );
+          })
+        )}
+      </div>
+    </MainLayout>
   );
- 
 }
 
 export default Home;
